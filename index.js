984ca4cf7d370fb65b51212308467f8db5abfcff
Mukul sehrawat <mukulsehrawat999@gmail.com>
2026-03-18 09:40:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -91,6 +91,91 @@ const updateButtons = () => {
     const buttons = document.getElementById(".watchlist-btn")
     
     buttons.forEach((btn) => {
-        
+        const isInWatclist = watchlist.find((m) => m.imdbID === btn.dataset.id)
+        if (isInWatclist) {
+            btn.classlist.add("remove")
+            btn.innerHTML = `${removeIcon} Remove`
+        }
     })
+}
+
+// add/remove localstorage when button clicked
+
+document.addEventListener("click", (e) => {
+    const btn = e.target.closest(".watchlist-btn")
+    
+    if (btn) {
+        const id = btn.dataset.id
+        const watchlist = JSON.parse(localStorage.getItem("watchlist")) || []
+        const movieObject = searchResults.find((m) => m.imdbID === id)
+        
+        btn.classlist.toggle("remove")
+        
+        if (btn.classlist.contains("remove")) {
+            if (movieObject) {
+                watchlist.push(movieObject)
+                localStorage.setItem("watchlist", JSON.stringify(watchlist))
+            }
+            btn.innerHTML = `${removeIcon} Remove`
+        } else {
+            const updated = watchlist.filter((m) => m.imdbID !== id)
+            localStorage.setItem("watchlist", JSON.stringify(updated))
+             btn.innerHTML = `${addIcon} Watchlist`
+             
+             // rerender if on watchlist page so removals vanish instantly
+             
+             if (watchlistContainer) {
+                renderWatchlist()
+             }
+        }
+    }
+})
+
+const renderWatchlist = () => {
+    const watchlist = JSON.parse(localStorage.getItem("watchlist")) || []
+    
+    // display message if watchlist is empty
+    
+    if (watchlist.length === 0) {
+        watchlistContainer.innerHTML = `<div class="message">
+    <img src="imgs/pixl3.gif" alt="cute green ghost" />
+          Your watchlist is looking a little empty...
+        </div>`
+        return
+    }
+    
+    watchlistContainer.innerHTML = watchlist.map((data) => 
+            ` <article class="film-card">
+        <div class="left-container">
+            <img
+            src="${data.Poster}"
+            alt="${data.Title} poster"
+            onerror="this.src = 'noimg.jpg'"
+            />
+
+            <button class="watchlist-btn" data-id="${data.imdbID}">
+            ${addIcon} Watchlist
+            </button>
+        </div>
+        <div class="film-info">
+            <div class="film-title-row">
+            <h2>${data.Title}</h2>
+            <span class="rating">${starIcon} ${data.imdbRating} </span>
+            </div>
+            <div class="meta-row">
+            <span class="year">${data.Year}</span>
+            <span class="dot"></span>
+            <span class="runtime">${data.Runtime}</span>
+            <span class="dot"></span>
+            <span class="genres">${data.Genre}</span>
+            </div>
+            <p class="synopsis">${data.Plot}</p>
+        </div>
+        </article>`,).join("")
+        
+        updateButtons()
+}
+
+if (watchlistContainer) {
+    renderWatchlist()
 }
